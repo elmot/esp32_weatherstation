ab5880800c6260f3430e452a00e05ba8a57c5c08
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2024-07-11 13:51:56 +0300

    Minor things

--- main/main.cpp
+++ main/main.cpp
@@ -11,15 +11,15 @@ void app_main() {
     inky.init_hw();
     inky.set_border(InkyImpression7_3f::BLUE);
     inky.set_background(image1.data());
-    for (int i = 100; i <400; ++i) {
-        inky.set_pixel(200 + i, 50 + i, InkyImpression7_3f::WHITE);
-        inky.set_pixel(201 + i, 50 + i, InkyImpression7_3f::YELLOW);
-        inky.set_pixel(202 + i, 50 + i, InkyImpression7_3f::ORANGE);
-        inky.set_pixel(203 + i, 50 + i, InkyImpression7_3f::RED);
-        inky.set_pixel(204 + i, 50 + i, InkyImpression7_3f::GREEN);
-        inky.set_pixel(205 + i, 50 + i, InkyImpression7_3f::BLUE);
-        inky.set_pixel(206 + i, 50 + i, InkyImpression7_3f::BLACK);
-    }
+//    for (int i = 100; i <400; ++i) {
+//        inky.set_pixel(200 + i, 50 + i, InkyImpression7_3f::WHITE);
+//        inky.set_pixel(201 + i, 50 + i, InkyImpression7_3f::YELLOW);
+//        inky.set_pixel(202 + i, 50 + i, InkyImpression7_3f::ORANGE);
+//        inky.set_pixel(203 + i, 50 + i, InkyImpression7_3f::RED);
+//        inky.set_pixel(204 + i, 50 + i, InkyImpression7_3f::GREEN);
+//        inky.set_pixel(205 + i, 50 + i, InkyImpression7_3f::BLUE);
+//        inky.set_pixel(206 + i, 50 + i, InkyImpression7_3f::BLACK);
+//    }
     inky.show();
     while (true) {
         puts("End_loop");
